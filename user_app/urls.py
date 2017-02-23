27ef130d567cec11b270d8f15eae1adc4c67294c
RyanHelling <rahelling@yahoo.com>
2017-02-22 19:17:18 -0800

    log in working

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from user_app.views import signup_page, profile
+from user_app.views import signup_page, profile, signup
 
 urlpatterns = [
-    url(r'^signup', signup_page, name="signup_page"),
-    url(r'^profile', profile, name="profile")
+    url(r'^signup_page', signup_page, name="signup_page"),
+    url(r'^profile/(?P<user_id>\d+)/', profile, name="profile"),
+    url(r'^signup', signup, name="signup" )
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,17 +1,21 @@
 import datetime
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.template import loader
 from django.template import RequestContext
 
-
 # Create your views here.
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_protect
+
 
 def signup_page(request):
-    template = loader.get_template('user_app/signup_page.html')
-    template_profile = loader.get_template('user_app/profile.html')
+    return render(request, "user_app/signup_page.html")
 
+
+def signup(request):
+    template = loader.get_template('user_app/profile.html')
     if request.method == "POST":
 
         username = request.POST['username']
@@ -20,20 +24,24 @@ def signup_page(request):
         last_name = request.POST['last_name']
         email = request.POST['email']
         kwargs = {
-            'username':username, 'password':password, 'first_name':first_name,
-            'last_name':last_name, 'email':email
+            'username': username, 'password': password, 'first_name': first_name,
+            'last_name': last_name, 'email': email
         }
         new_user = User.objects.create(**kwargs)
         new_user.save()
-
         context = {
-            'text':"POST", 'first_name':first_name
+            'first_name': first_name, 'last_name': last_name, 'username': username
         }
-
-        return HttpResponse(template_profile.render(RequestContext(request), context))
+        user_id = new_user.pk
+        return HttpResponseRedirect(reverse('profile', args=(user_id,)))
     else:
         return HttpResponse(template.render(request))
 
 
-def profile(request):
-    return HttpResponse(RequestContext(request))
+def profile(request, user_id):
+    logged_in_user = User.objects.get(pk=user_id)
+    context = {
+        'logged_in_user': logged_in_user
+    }
+    template = loader.get_template('user_app/profile.html')
+    return HttpResponse(template.render(context, request))
